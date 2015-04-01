e5e2e49cde51cdedf0a300e41d697155b42e3998
jurasikmaniac <jurasikmaniac@gmail.com>
2015-04-01 11:15:57 +0600

    1

--- sort_graph/sort_graph/Form1.Designer.cs
+++ sort_graph/sort_graph/Form1.Designer.cs
@@ -195,6 +195,7 @@
             this.button1.TabIndex = 6;
             this.button1.Text = "Merge";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
